4b07903ba79e3fec07594b64170b75ec0d193940
Cherian Hormis <cheriankhormis@gmail.com>
2023-08-21 13:49:44 +1000

    Update streamlit_app.py
    added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
-streamlit.header('Breakfast Menu')
 
-   
+streamlit.header('Breakfast Menu')  
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
